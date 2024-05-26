d680889bb26089d90a34470b1652266aae9e2fd4
JANANI RAJENDRAN <140250422+janani06rajendran@users.noreply.github.com>
2024-05-26 18:35:07 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // pizza basepizza = new pizza(false);
+        // basepizza.addExtracheese();
+        // basepizza.addExtraToppings();
+        // basepizza.takeAway();
+        // basepizza.getBill();
+        Deluxpizza dp = new Deluxpizza(false);
+        dp.getBill();
+    }
+}
---
+++ Deluxpizza.java
@@ -0,0 +1,8 @@
+public class Deluxpizza extends pizza {
+    public Deluxpizza(Boolean veg) {
+        super(veg);
+        super.addExtracheese();
+        super.addExtraToppings();
+    }
+    
+}
